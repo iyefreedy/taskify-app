840391b2a2c2df9fda0810ffb3deec1ae6ed7dba
Muhammad Quraisy <iye.fredickson@gmail.com>
2025-01-12 21:31:16 +0700

    feat: add Loading component with Backdrop and CircularProgress for improved user experience during data loading

---
+++ app/loading.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+
+export default function Loading() {
+  return (
+    <>
+      <Backdrop
+        sx={(theme) => ({ color: "#fff", zIndex: theme.zIndex.drawer + 1 })}
+        open
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </>
+  );
+}
